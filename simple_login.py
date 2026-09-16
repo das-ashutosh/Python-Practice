134c9b9ace30a368591626ca2ffb0014a8b2c0da
ASHUTOSH DAS <connect.ashutoshdas@gmail.com>
2026-09-16 16:09:59 +0530

    Implement simple login functionality

---
+++ simple_login.py
@@ -0,0 +1,9 @@
+username = "ashutosh"
+password = "python123"
+
+user = input("Enter username: ")
+pwd = input("Enter password: ")
+if user == username and pwd == password:
+    print("Login successful!")
+else:
+    print("Invalid username or password.")
